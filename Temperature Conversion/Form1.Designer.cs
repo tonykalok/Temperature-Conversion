17fa93ba3818ffb969aa42b25bf8b3713b2e24f7
Tony To <tonykalok@gmail.com>
2022-11-06 17:06:37 +0800

    Disable Minimize Maximize Box

--- Temperature Conversion/Form1.Designer.cs
+++ Temperature Conversion/Form1.Designer.cs
@@ -83,6 +83,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Temperature Conversion";
